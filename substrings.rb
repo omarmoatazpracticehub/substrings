21760200c4a086586222988c7ee61c44f1a28e75
Omar M. Attia <omarmoataz@outlook.com>
2017-08-26 04:46:46 +0200

    first commit

---
+++ substrings.rb
@@ -0,0 +1,34 @@
+def split_word s
+  indices = (0...s.length).to_a
+  variations = indices.product(indices).reject{|i,j| i > j}.map{|i,j| s[i..j]}.uniq
+  return variations
+end
+
+def string_cleanup (string)
+	modded_string = ""
+	string.each_char do |x|
+		if x =~ /[a-zA-Z]/
+			modded_string += x
+		end
+	end
+end
+
+def substrings(string, dictionary)
+	hash_listing = Hash.new(0)
+	string = string.split(" ")
+	puts string.inspect
+	string.each do |element|
+		element = string_cleanup(element)
+		elements = split_word(element)
+		elements.each do |x|
+			if dictionary.index(x.downcase) != nil
+				hash_listing[x] += 1;
+			end
+		end
+	end
+	return hash_listing
+end
+
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
